df55837d0bc69b439fb2dd4172a7974fb0b4212b
7paradox <31220459+7paradox@users.noreply.github.com>
2020-04-16 17:43:26 +0530

    without dtos but working

--- src/main/java/com/covid19logger/Controller/Covid19Controller.java
+++ src/main/java/com/covid19logger/Controller/Covid19Controller.java
@@ -1,19 +1,12 @@
 package com.covid19logger.Controller;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import com.covid19logger.Manager.Manager;
-
 import com.covid19logger.model.SymDetails;
 import com.covid19logger.model.UserDetails;
 import com.covid19logger.model.UserSymptoms;
@@ -29,7 +22,7 @@ public class Covid19Controller {
 
 
 	@PostMapping("/usersymptoms")
-	public ResponseEntity<List<UserSymptoms>> userSymptoms(@RequestBody SymDetails symdetails) {
+	public ResponseEntity<UserSymptoms> userSymptoms(@RequestBody SymDetails symdetails) {
 		System.out.println(symdetails.getDate());
 
 			return ResponseEntity.ok(manager.save(symdetails));
--- src/main/java/com/covid19logger/Dao/SymptomsDao.java
+++ src/main/java/com/covid19logger/Dao/SymptomsDao.java
@@ -1,40 +1,11 @@
 package com.covid19logger.Dao;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
-
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import com.covid19logger.model.UserSymptoms;
 
 public interface SymptomsDao extends JpaRepository<UserSymptoms, Long> {
-
-	List<UserSymptoms> findAllByDate(Date date);
-
-	/*
-	 * @Query("select s from symptoms s where s.date like '%:date'") Date
-	 * findByDate(int date);
-	 * 
-	 * @Modifying
-	 * 
-	 * @Query("UPDATE symptoms s SET s.") int updateDate(Date date);
-	 */
-
-
-
-	int deleteByDate(Date date);
-	@Transactional
-	@Modifying
-	int deleteByDateAndUserID(LocalDate date,Long userId);
-	
-	@Transactional
-	UserSymptoms findAllByDateAndUserID(Date date, long userID);
 
 	Optional<UserSymptoms> findByUserID(long userID);
 
